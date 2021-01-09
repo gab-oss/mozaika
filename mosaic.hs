0084a8a9a3a57c1789a3c37ff765537cbbdce1c5
Gabriela Ossowska <gab.ossowska@gmail.com>
2021-01-09 16:48:57 +0100

    count how many cells are painted around a clue

--- mosaic.hs
+++ mosaic.hs
@@ -19,8 +19,17 @@ checkCell [] _ = [] -- zamalowuje x-ta komorke w wierszu
 checkCell (cell : row) 0 = ['#'] ++ row
 checkCell (cell : row) x = [cell] ++ checkCell row (x - 1)  
 
+cutCellGroup mosaic (y,x) = map (take (x2 - x1 + 1)) (map (drop x1) (take (y2 - y1 + 1) (drop y1 mosaic))) 
+                                    where y1 = if (y - 1 < 0) then y else (y - 1)
+                                          y2 = if (y + 1 >= (length $ mosaic)) then y else (y + 1)
+                                          x1 = if (x - 1 < 0) then x else (x - 1)
+                                          x2 = if (x + 1 >= (length $ head(mosaic))) then x else (x + 1)
+
+countCheckedForClue mosaic (y,x) = sum (map length (map (filter (\n -> n == '#')) (cutCellGroup mosaic (y,x))))
+
 -- solve puzzle = print ( checkCell (head (emptyMosaic puzzle)) 3 )
-solve puzzle = print (checkNext (emptyMosaic puzzle) (1,3))
+-- solve puzzle = print (checkNext (emptyMosaic puzzle) (1,3))
+solve puzzle = print (countCheckedForClue (checkNext (emptyMosaic puzzle) (1,3)) (1,3))
 
 main = do puzzle <- readPuzzle "puzzle"
           putStrLn (show $ length $ puzzle) -- wyświetl liczbę wierszy łamigłówki
