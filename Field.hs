5989ab7419fa47abe3793962ed966f929d6c1326
alobach <toni9619.al@gmail.com>
2021-01-11 23:20:51 +0100

    fieald module

---
+++ Field.hs
@@ -0,0 +1,27 @@
+module Field where 
+
+import Data.Char
+
+type FieldValue = Maybe Int
+data State = Filled | Empty | Null deriving(Eq,Show)
+
+{-
+    State : Stan pola
+    FieldValue : wartość pola
+    Bool : czy zostało już sprawdzone --TODO: nie wiem czy jest potrzebne ponniewż można ustawić state na NUll np   
+-}
+data Field = Field State FieldValue Bool deriving Show 
+
+getState :: Field -> State
+getState(Field s _ _) = s 
+
+getValue :: Field -> FieldValue
+getValue(Field _ v _) = v 
+
+getFieldProcessedStatus :: Field -> Bool
+getFieldProcessedStatus(Field _ _ b) = b 
+
+toDefaultField :: Char -> Field -- TODO: parse z pliczku uwzględniając format
+toDefaultField '.' = Field Null Nothing False 
+toDefaultField c = Field Null (Just (digitToInt c)) False 
+
---
+++ Solver.hs
@@ -0,0 +1,7 @@
+import Data.List
+import System.IO
+import Field
+
+
+main = do
+    print "DUPA"
